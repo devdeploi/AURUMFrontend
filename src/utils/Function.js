e7849d6985760f8700f4856e26985e39db8caad2
Apple <apple@Apples-Mac-mini.lan>
2026-02-12 19:34:36 +0530

    Added Basic

--- src/utils/Function.js
+++ src/utils/Function.js
@@ -2,6 +2,10 @@ export const APIURL = window.location.hostname === "localhost" ? "http://localho
 
 export const BASEURL = window.location.hostname === "localhost" ? "http://localhost:8000" : "https://backendaurum.safprotech.com";
 
+
+// export const APIURL = "https://backendaurum.safprotech.com/api";
+// export const BASEURL = "https://backendaurum.safprotech.com";
+
 export const onError = (error) => {
     error.target.src = "images/AURUM.png"
 }
